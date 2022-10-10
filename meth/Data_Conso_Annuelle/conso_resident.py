85dc4dbb7e5c34b0b0a9858a23affc4b8a507baf
Thomas Laugie <114211473+ThomasLaugie@users.noreply.github.com>
2022-10-10 14:39:37 +0200

    Update conso_resident.py

--- meth/Data_Conso_Annuelle/conso_resident.py
+++ meth/Data_Conso_Annuelle/conso_resident.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv("consommation-annuelle-residentielle-par-adresse.csv",sep=';')
+df = pd.read_csv(
+    "consommation-annuelle-residentielle-par-adresse.csv",sep=';')
 cons_total = "Consommation annuelle totale de l'adresse (MWh)"
 cons_moyen = "Consommation annuelle moyenne par logement de l'adresse (MWh)"
 cons_commune = "Consommation annuelle moyenne de la commune (MWh)"
@@ -9,6 +10,50 @@ df_2018=df[df['Année'] == 2018]
 df_2019=df[df['Année'] == 2019]
 df_2020=df[df['Année'] == 2020]
 df_2021=df[df['Année'] == 2021]
+
+# Commune qui consomme le plus dans l'année:
+df_max = df_2018[cons_commune].max()
+df_max_ville_2018 = df_2018[df_2018[cons_commune] == df_max]
+df_max_ville_2018 = df_max_ville_2018['Nom de la commune'].unique().tolist()
+df_max_ville_2018.append(df_max)
+
+df_max = df_2019[cons_commune].max()
+df_max_ville_2019 = df_2019[df_2019[cons_commune] == df_max]
+df_max_ville_2019 = df_max_ville_2019['Nom de la commune'].unique().tolist()
+df_max_ville_2019.append(df_max)
+
+df_max = df_2020[cons_commune].max()
+df_max_ville_2020 = df_2020[df_2020[cons_commune] == df_max]
+df_max_ville_2020 = df_max_ville_2020['Nom de la commune'].unique().tolist()
+df_max_ville_2020.append(df_max)
+
+df_max = df_2021[cons_commune].max()
+df_max_ville_2021 = df_2021[df_2021[cons_commune] == df_max]
+df_max_ville_2021 = df_max_ville_2021['Nom de la commune'].unique().tolist()
+df_max_ville_2021.append(df_max)
+
+# Commune qui consomme le moins dans l'année:
+
+df_min = df_2018[cons_commune].min()
+df_min_ville_2018 = df_2018[df_2018[cons_commune] == df_min]
+df_min_ville_2018 = df_min_ville_2018['Nom de la commune'].unique().tolist()
+df_min_ville_2018.append(df_min)
+
+df_min = df_2018[cons_commune].min()
+df_min_ville_2019 = df_2019[df_2019[cons_commune] == df_min]
+df_min_ville_2019 = df_min_ville_2019['Nom de la commune'].unique().tolist()
+df_min_ville_2019.append(df_min)
+
+df_min = df_2020[cons_commune].min()
+df_min_ville_2020 = df_2020[df_2020[cons_commune] == df_min]
+df_min_ville_2020 = df_min_ville_2020['Nom de la commune'].unique().tolist()
+df_min_ville_2020.append(df_min)
+
+df_min = df_2021[cons_commune].min()
+df_min_ville_2021 = df_2021[df_2021[cons_commune] == df_min]
+df_min_ville_2021 = df_min_ville_2021['Nom de la commune'].unique().tolist()
+df_min_ville_2021.append(df_min)
+
 
 def Elec_Adresse(Commune,Adresse,Annee,type):
     """"Donne la consommation annuelle de cette adresse en MHW
