dcce2c45ec839310646a7cef4b444bb3d7242233
HAMZA <114219430+slamaGit@users.noreply.github.com>
2022-11-30 07:43:55 +0100

    Update Modèle.py

--- meth/prediction/Modèle.py
+++ meth/prediction/Modèle.py
@@ -15,14 +15,14 @@ import statsmodels.api as sm
 #muting unnecessary warnings if needed
 import warnings
 # %%____________________________________________-
-ts = cr.dataframe() # data from 2019-01-01 00:00:00 to 2022-11-14 23:45:00
+df = cr.dataframe() # data from 2019-01-01 00:00:00 to 2022-11-29 12:45:00
 
 # %%
 #splitting time series to train and test subsets
-y_test = ts.iloc[-35880:,:].copy()
+data_test = ts.iloc[-35880:,:].copy()
 #Unobserved Components model definition
 
- = sm.tsa.UnobservedComponents(ts,
+ model_UC1= sm.tsa.UnobservedComponents(df,
                                         level='dtrend',
                                         irregular=True,
                                         stochastic_level = False,
@@ -37,7 +37,6 @@ model_UC1res = model_UC1.fit()
 #printing statsmodels summary for model
 print(model_UC1res.summary())
 
-print("")
 
 #calculating mean absolute error and root mean squared error for in-sample prediction of model
 print(f"In-sample mean absolute error (MAE): {'%.0f' % model_UC1res.mae}, In-sample root mean squared error (RMSE): {'%.0f' % np.sqrt(model_UC1res.mse)}")
@@ -46,7 +45,7 @@ print(f"In-sample mean absolute error (MAE): {'%.0f' % model_UC1res.mae}, In-sam
 forecast_UC1 = model_UC1res.forecast(steps=35880)
 
 #calculating mean absolute error and root mean squared error for out-of-sample prediction for model evaluation
-error = np.sqrt(np.mean([(y_test.iloc[x,:] - forecast_UC1[x]) ** 2 for x in range(len(forecast_UC1))]))      
+error = np.sqrt(np.mean([(data_test.iloc[x,:] - forecast_UC1[x]) ** 2 for x in range(len(forecast_UC1))]))      
 print(f"Out-of-sample root mean squared error (RMSE): {'%.0f' % error}")
 # %%__________________
 #plotting residual diagnostics of Unobserved Components model
@@ -57,14 +56,14 @@ forc= model_UC1res.predict(start="2022-12-08 00:00:00", end="2022-12-08 23:45:00
 forc.plot()
 # %%
 
-pred = model_UC1res.predict(start="2021-11-06 06:00:00", end="2022-12-08 23:45:00")
+pred = model_UC1res.predict(start="2019-01-01 06:00:00", end="2022-12-08 23:45:00")
 f = plt.figure(figsize=(18,6),dpi=200);
 #setting title and size of title
 plt.suptitle('Unobserved Components model prediction Vs Real dataset', fontsize=20);
 #setting y axis label
 plt.ylabel('MW');
 #plotting trend component
-plt.plot(y_test, label='France Electric Power Energy consumption (MW)');
+plt.plot(df, label='France Electric Power Energy consumption (MW)');
 #plotting linear model of trend component
 plt.plot(pred, label='Unobserved Components model prediction');
 
