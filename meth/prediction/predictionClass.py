60b72dfddd166eb854776dea3b0fc969436487bb
hamza <hamzaslama82@gmail.com>
2022-12-06 22:40:43 +0100

    docs

--- meth/prediction/predictionClass.py
+++ meth/prediction/predictionClass.py
@@ -11,12 +11,18 @@ import seaborn as sns
 sns.set()
 
 class MODEL():
-
+    """cette classe définit le modèle de prédiction nommé Unobserved Components Model "UCM" et cette
+    se compose de deux fonctions la première c'est la fonction d'initialisation de l'objet  (__init__)
+    avec les attributs de cette fonction sont :\\
+      - start : c'est la date du début de prédiction de type date écrit sous la forme ANNÉE-MOIS exemple (2022-06)
+      - end : c'est la date du fin de prédiction de type date écrit sous la forme ANNÉE-MOIS exemple (2022-09).
+    la deuxième fonction nommé (mod) définit le modèle de prédiction UCM du premier temp cette fonction fait l'importation
+    du data collectés du 01 JAN 2019 jusqu'à 06 Déc 2022 et cette fonction retourne forc:\\
+      -for : c'est les valeure de prédiction cherché sous form data frame"""
     def __init__(self,start,end) -> None:
         self.start = start
         self.end = end
         pass
-
     def mod(self) :
         df =pd.read_csv("./dataset/dataelec.csv")  # data from 2019-01-01 00:00:00 to 2022-11-29 12:45:00
         df=df.set_index('Temps')
